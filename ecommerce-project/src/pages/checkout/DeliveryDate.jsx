6424bb36cfb6e6bafffac90445fca014bfec54a7
aditya sahu <tools.adityasahu@gmail.com>
2026-08-24 16:23:06 +0530

    Separate into smaller components

---
+++ ecommerce-project/src/pages/checkout/DeliveryDate.jsx
@@ -0,0 +1,7 @@
+export function DeliveryDate() {
+  return (
+    <div className="delivery-date">
+      Delivery date: Tuesday, June 21
+    </div>
+  );
+}
--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,5 +1,6 @@
-import { formatMoney } from '../../utils/money';
+import { CartItemDetails } from './CartItemDetails';
 import { DeliveryOptions } from './DeliveryOptions';
+import { DeliveryDate } from './DeliveryDate';
 
 export function OrderSummary({ cart, deliveryOptions }) {
   return (
@@ -7,34 +8,9 @@ export function OrderSummary({ cart, deliveryOptions }) {
       {cart.map((cartItem) => {
         return (
           <div key={cartItem.productId} className="cart-item-container">
-            <div className="delivery-date">
-              Delivery date: Tuesday, June 21
-            </div>
-
+            <DeliveryDate />
             <div className="cart-item-details-grid">
-              <img className="product-image"
-                src={cartItem.image} />
-
-              <div className="cart-item-details">
-                <div className="product-name">
-                  {cartItem.name}
-                </div>
-                <div className="product-price">
-                  {formatMoney(cartItem.priceCents)}
-                </div>
-                <div className="product-quantity">
-                  <span>
-                    Quantity: <span className="quantity-label">{cartItem.quantity}</span>
-                  </span>
-                  <span className="update-quantity-link link-primary">
-                    Update
-                  </span>
-                  <span className="delete-quantity-link link-primary">
-                    Delete
-                  </span>
-                </div>
-              </div>
-
+              <CartItemDetails cartItem={cartItem}/>
               <DeliveryOptions deliveryOptions={deliveryOptions} cartItem={cartItem} />
             </div>
           </div>
