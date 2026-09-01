6b34562a6c696811f1e2a35f559d9e13793a42a1
aditya sahu <tools.adityasahu@gmail.com>
2026-09-01 12:52:28 +0530

    Create Controlled input for the quantity textbox Use PUT http method

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -3,23 +3,30 @@ import axios from 'axios';
 import { formatMoney } from '../../utils/money';
 
 export function CartItemDetails({ cartItem, loadCart }) {
-  const[isUpdatingQuantity, setIsUpdatingQuantity] = useState(false);
-  
-  const updateQuantity = () => {
-    if(isUpdatingQuantity) {
-      setIsUpdatingQuantity(false);
-    } else {
-      setIsUpdatingQuantity(true);
-    }
-  }
+  const [isUpdatingQuantity, setIsUpdatingQuantity] = useState(false);
+  const [quantity, setQuantity] = useState(cartItem.quantity);
 
   // Safe navigation in case cartItem or product is undefined
   const product = cartItem?.product || {};
 
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`);
-    await loadCart(); 
+    await loadCart();
   }
+
+  const updateQuantity = async () => {
+    if (isUpdatingQuantity) {
+      await axios.put(`/api/cart-items/${cartItem.productId}`, {
+        quantity: Number(quantity),
+      });
+      await loadCart();
+      setIsUpdatingQuantity(false);
+    } else {
+      setIsUpdatingQuantity(true);
+    }
+  }
+
+
 
   return (
     <>
@@ -35,9 +42,9 @@ export function CartItemDetails({ cartItem, loadCart }) {
         </div>
         <div className="product-quantity">
           <span>
-            Quantity: {isUpdatingQuantity 
-              ? <input type="text" className="quantity-textbox" />
-              : <span className="quantity-label" style={{display: isUpdatingQuantity ? 'none' : 'block'}}>{cartItem.quantity}</span>}
+            Quantity: {isUpdatingQuantity
+              ? <input type="text" className="quantity-textbox" value={quantity} onChange={(event) => setQuantity(event.target.value)} />
+              : <span className="quantity-label" style={{ display: isUpdatingQuantity ? 'none' : 'block' }}>{cartItem.quantity}</span>}
           </span>
           <span className="update-quantity-link link-primary" onClick={updateQuantity}>
             Update
