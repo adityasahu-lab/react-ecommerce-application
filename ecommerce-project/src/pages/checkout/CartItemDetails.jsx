a00743ef72b3fa6bb43e63d3bf4ad14d1b58f911
aditya sahu <tools.adityasahu@gmail.com>
2026-09-01 11:10:01 +0530

    Create textbox

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -24,7 +24,9 @@ export function CartItemDetails({ cartItem, loadCart }) {
         </div>
         <div className="product-quantity">
           <span>
-            Quantity: <span className="quantity-label">{cartItem.quantity}</span>
+            Quantity:
+              <input type="text" className="textbox" />
+             <span className="quantity-label">{cartItem.quantity}</span>
           </span>
           <span className="update-quantity-link link-primary">
             Update
