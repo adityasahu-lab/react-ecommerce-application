89b8ee2e2b5e92ed7f727e724a22561e66a839b5
aditya sahu <tools.adityasahu@gmail.com>
2026-09-07 17:36:04 +0530

    Test axios.post was called with the correct value

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -75,9 +75,21 @@ describe('Product component', () => {
     render(<Product product={product} loadCart={loadCart} />);
     
     const quantitySelector = screen.getByTestId('product-quantity-selector');
+     const addToCartButton = screen.getByTestId("add-to-cart-button");
     const user = userEvent.setup();
+   
     await user.selectOptions(quantitySelector, '3');
+    await user.click(addToCartButton);
 
     expect(quantitySelector).toHaveValue('3');
+
+    expect(axios.post).toHaveBeenCalledWith(
+       '/api/cart-items',
+      {
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 3
+      }
+    );
+    expect(loadCart).toHaveBeenCalled();
   });
 });
