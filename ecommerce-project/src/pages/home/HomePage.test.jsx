84e2ed17b135010ed0c53c4800b25cd108062f5b
aditya sahu <tools.adityasahu@gmail.com>
2026-09-08 12:50:38 +0530

    Test Add to Cart button works

--- ecommerce-project/src/pages/home/HomePage.test.jsx
+++ ecommerce-project/src/pages/home/HomePage.test.jsx
@@ -2,15 +2,18 @@ import { it, expect, describe, vi, beforeEach } from 'vitest';
 import { render, screen, within } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 import axios from 'axios';
+import userEvent from '@testing-library/user-event';
 import { HomePage } from './HomePage';
 
 vi.mock('axios');
 
 describe('HomePage component', () => {
   let loadCart;
+  let user;
 
   beforeEach(() => {
     loadCart = vi.fn();
+    user = userEvent.setup();
 
     axios.get.mockImplementation(async (urlPath) => {
       if (urlPath === '/api/products') {
@@ -59,7 +62,38 @@ describe('HomePage component', () => {
     expect(
       within(productContainers[1]).getByText('Intermediate Size Basketball')
     ).toBeInTheDocument();
-    
+  });
+
+  it('checks if the Add to Cart buttons work', async () => {
+    render(
+    <MemoryRouter>
+      <HomePage cart={[]} loadCart={loadCart} />  
+    </MemoryRouter>
+    );
+
+    const productContainers = await screen.findAllByTestId('product-container');
+    // const addToCartButton = screen.getByTestId("add-to-cart-button");
+
+    const addToCartButton1 = within(productContainers[0]).getByTestId("add-to-cart-button");
+    await user.click(addToCartButton1);
+
+    const addToCartButton2 = within(productContainers[1]).getByTestId("add-to-cart-button");
+    await user.click(addToCartButton2);
+
+    expect(axios.post).toHaveBeenNthCalledWith(1, '/api/cart-items',
+      {
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1
+      });
+
+    expect(axios.post).toHaveBeenNthCalledWith(2, '/api/cart-items',
+      {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1
+      });
+
+    expect(loadCart).toHaveBeenCalledTimes(2);
+
   });
 });
 
